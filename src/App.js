eb0b0ecdf8535c68cb6f59ffb6917911aa40ca33
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-03-01 11:03:39 +0100

    reactjs

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import './App.css';
+import Home from './screens/Home';
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const [user, setUser] = useState()
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    fetch(`./src/php/auth.php`,{
+      method:'POST',
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username, password}) //convert data to JSON
+  })
+    .then((response)=>response.json())
+    .then((response)=>{
+      console.log(response[0].Message);
+    })
+    .catch((error)=>{
+        console.log("Error Occured" + error);
+    });
+  };
+  
+  if (user) {
+    return <Home />
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username: </label>
+      <input
+        type="text"
+        value={username}
+        placeholder="enter a username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <div>
+        <label htmlFor="password">password: </label>
+        <input
+          type="password"
+          value={password}
+          placeholder="enter a password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default App;
