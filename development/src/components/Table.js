911b038ed4d6908c64e74c97d163a3498a9b571f
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-03-03 12:55:08 +0100

    table inventario

--- development/src/components/Table.js
+++ development/src/components/Table.js
@@ -1,26 +1,46 @@
-import { React } from 'react';
+import React from 'react';
+import '../assets/css/table.css';
+import search from '../assets/img/search.png';
 
 export default function List({data}) {
     if(!data || data.length===0){
         return (<p>No hay inventario para mostrar...</p>);
     }
+    const keys = Object.keys(data[0]);
+
+    const Item = ({itemData}) => (
+        Object.values(itemData).map((value, i) => (
+            <p key={i}>{value}</p>
+        ))
+    );
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    {/* {data.map((text, id) => <th key={id} >{text}</th>)} */}
-                    {Object.keys(data).map((item) => (
-                        <th>{item}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item) => (
-                    <tr key={item.id}>
-                        {item.map((value) => (<td>{value}</td>))}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <>
+        <section className='tableOptions'>
+          <div className='searchBar'>
+            <input type="search" placeholder='Búsqueda'/>
+            <img src={search} alt="icono lupa"/>
+          </div>
+          <div className='sortBy'>
+            <label htmlFor='sortBy'>Ordenar por:</label>
+            <select name='sortBy'>
+                <option value="1">Uno</option>
+                <option value="2">Dos</option>
+            </select>
+          </div>
+        </section>
+        <div>
+            <b>
+                {keys.map((value) => (<span key={value}>{value}</span>))}
+            </b>
+        </div>
+        <ul>
+            {data.map((item)=>(
+                <li key={item.id}>
+                    <Item itemData={item} />
+                </li>
+            ))}
+        </ul>
+        </>
     );
 }
--- development/src/screens/Inventario.js
+++ development/src/screens/Inventario.js
@@ -2,30 +2,40 @@ import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Table from "../components/Table";
 
+const DATA = [
+    {id: 1, name: "Jose", company: "Capgeminy"},
+    {id: 2, name: "Laura", company: "Soltech"},
+    {id: 3, name: "Paco", company: "Capgeminy"},
+    {id: 4, name: "Almudena", company: "IrisTC"},
+];
+
 export default function Inventario() {
 
   const [inventory, setInventory] = useState(null);
 
   useEffect(() => {
-    fetch('http://api.gomaespumaycojines.es/getInventory.php',{
-      method:'GET',
-      withCredentials: true,    
-      crossorigin: true,    
-      mode: 'no-cors',
-      headers:{
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setInventory(data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  },[]);
+    setInventory(DATA);
+  }, []);
+  // useEffect(() => {
+  //   fetch('http://api.gomaespumaycojines.es/getInventory.php',{
+  //     method:'GET',
+  //     withCredentials: true,    
+  //     crossorigin: true,    
+  //     mode: 'no-cors',
+  //     headers:{
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data);
+  //     setInventory(data);
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+  // },[]);
 
   return (
     <>
@@ -33,16 +43,6 @@ export default function Inventario() {
       <main>
         <button className="primary">Crear modelo</button>
         <button className="primary">Añadir inventario</button>
-        <div>
-          <div>
-            <p>Búsqueda rápida:</p>
-            <input type="search" />
-          </div>
-          <div>
-            <p>Ordenar por:</p>
-            {/*  */}
-          </div>
-        </div>
         <Table data={inventory} />
       </main>
     </>
