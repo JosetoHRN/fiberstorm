8990f33ede9d8317f06a45fe045ed893fd91b1c2
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-03-13 22:55:29 +0100

    Update post.php

--- php/inventario/post.php
+++ php/inventario/post.php
@@ -3,41 +3,28 @@
     $database = new Database();
     $conn = $database->getConnection();
 
+    if(isset($_POST['submit'])) {
+        $fileName_to_insert = 'inventario_default.png';
+        if($_FILES['imagen']['name'] != "" && $_FILES['imagen']['size'] > 0) {
+            $uploads_dir = '../../assets/img/inventario/';
+            $target_file = $uploads_dir . $_FILES["imagen"]["name"];
+            if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
+                $fileName_to_insert = $_FILES["imagen"]["name"];
+            }
+        }
+        
 
-    if($_FILES['imagen']['name'] != "" && $_FILES['imagen']['size'] > 0) {
-        echo $_FILES['imagen']['name'];
-        echo $_FILES['imagen']['type'];
-        echo $_FILES['imagen']['size'];
-        echo $_FILES['imagen']['error'];
-    }else{
-        echo "no upload";
+        $modelo = $_POST['modelo'];
+        $tipo = $_POST['tipo'];
+        $ref = isset($_POST['referencia']) ? $_POST['referencia'] : '-' ;
+        // $imagen = $_FILES['imagen']['name'];
+        $importancia = $_POST['importancia'];
+        $estado = isset($_POST['estado']) ? $_POST['estado'] : '-' ;
+        $cantidad = $_POST['cantidad'];
+        $sql = "INSERT INTO inventario (modelo, tipo, ref, imagen, importancia, estado, cantidad) VALUES ($modelo, $tipo, $ref, $fileName_to_insert, $importancia, $estado, $cantidad);";
+        $sentencia = $conn->prepare($sql);
+        $sentencia->execute();
+    
+        header("Location: ../../home/inventario.php");
     }
-    // if(isset($_POST["submit"])) {
-    //     $modelo = $_POST['modelo'];
-    //     $tipo = $_POST['tipo'];
-    //     $ref = isset($_POST['referencia']) ? $_POST['referencia'] : '-' ;
-    //     // $imagen = $_FILES['imagen']['name'];
-    //     $importancia = $_POST['importancia'];
-    //     $estado = isset($_POST['estado']) ? $_POST['estado'] : '-' ;
-    //     $cantidad = $_POST['cantidad'];
-
-    //     $uploads_dir = '../../assets/img/inventario/';
-    //     $target_file = $uploads_dir . basename($_FILES["imagen"]["name"]);
-    //     if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
-    //         echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    //     } else {
-    //         echo "Sorry, there was an error uploading your file.";
-    //     }
-    // }
-
-    
-
-    
-
-	// $sql = "INSERT INTO inventario (modelo, tipo, ref, imagen, importancia, estado, cantidad) VALUES ($modelo, $tipo, $ref, $imagen, $importancia, $estado, $cantidad);";
-    // $sentencia = $conn->prepare($sql);
-    // $sentencia->execute();
-	// $inv = $sentencia->fetchAll(PDO::FETCH_ASSOC);
-    
-    // echo json_encode($inv);
 ?>
