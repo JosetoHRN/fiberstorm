193f882519874ff69414a12d49ed974347ade153
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-03-14 12:49:31 +0100

    Update delete.php

--- php/inventario/delete.php
+++ php/inventario/delete.php
@@ -13,16 +13,12 @@
         $img_name = $data['imagen'];
 
         $archivo_a_borrar = "../../assets/img/inventario/$img_name";
-        if (!unlink($archivo_a_borrar)) {
-            echo "Error al borrar archivo";
-        } else {
-            echo "Archivo borrado correctamente";
-        }
+        unlink($archivo_a_borrar);
 
-        // $sql = "DELETE FROM inventario WHERE id = $id;";
-        // $sentencia = $conn->prepare($sql);
-        // $sentencia->execute();
-        // echo "Elemento eliminiado con éxito.";
+        $sql = "DELETE FROM inventario WHERE id = $id;";
+        $sentencia = $conn->prepare($sql);
+        $sentencia->execute();
+        echo "Elemento eliminiado con éxito.";
     } catch (Exception $e) {
         echo 'Error: '.$e;
     }
