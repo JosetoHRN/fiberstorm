302c81031ddfd5ac60817f58b0f75a0312f15cf9
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2023-03-14 11:01:38 +0100

    Update post.php

--- php/inventario/post.php
+++ php/inventario/post.php
@@ -4,27 +4,30 @@
     $conn = $database->getConnection();
 
     if(isset($_POST['submit'])) {
-        $fileName_to_insert = 'inventario_default.png';
-        if($_FILES['imagen']['name'] != "" && $_FILES['imagen']['size'] > 0) {
-            $uploads_dir = '../../assets/img/inventario/';
-            $target_file = $uploads_dir . $_FILES["imagen"]["name"];
-            if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
-                $fileName_to_insert = $_FILES["imagen"]["name"];
+        try {
+            $fileName_to_insert = 'inventario_default.png';
+            if($_FILES['imagen']['name'] != "" && $_FILES['imagen']['size'] > 0) {
+                $uploads_dir = '../../assets/img/inventario/';
+                $target_file = $uploads_dir . $_FILES["imagen"]["name"];
+                if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
+                    $fileName_to_insert = $_FILES["imagen"]["name"];
+                }
             }
+
+            $modelo = $_POST['modelo'];
+            $tipo = $_POST['tipo'];
+            $ref = isset($_POST['referencia']) ? $_POST['referencia'] : '-' ;
+            $importancia = $_POST['importancia'];
+            $estado = isset($_POST['estado']) ? $_POST['estado'] : '-' ;
+            $cantidad = $_POST['cantidad'];
+
+            $sql = "INSERT INTO inventario (modelo, tipo, ref, imagen, importancia, estado, cantidad) VALUES ($modelo, $tipo, $ref, $fileName_to_insert, $importancia, $estado, $cantidad);";
+            $sentencia = $conn->prepare($sql);
+            $sentencia->execute();
+        } catch (Exception $e) {
+            echo 'Error: '.$e;
         }
         
-
-        $modelo = $_POST['modelo'];
-        $tipo = $_POST['tipo'];
-        $ref = isset($_POST['referencia']) ? $_POST['referencia'] : '-' ;
-        $importancia = $_POST['importancia'];
-        $estado = isset($_POST['estado']) ? $_POST['estado'] : '-' ;
-        $cantidad = $_POST['cantidad'];
-
-        $sql = "INSERT INTO inventario (modelo, tipo, ref, imagen, importancia, estado, cantidad) VALUES ($modelo, $tipo, $ref, $fileName_to_insert, $importancia, $estado, $cantidad);";
-        $sentencia = $conn->prepare($sql);
-        $sentencia->execute();
-    
         header("Location: ../../home/inventario.php");
     }
 ?>
